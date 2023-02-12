3fa1d4eafb26628ccb953549b1fd9bbc9189448a
Thanis <77865201+thanis04@users.noreply.github.com>
2023-02-12 17:14:24 +0530

    RaiseButton doesn't work this flutter version

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Begin'),
+        ),
+        body: Column(
+          children: [
+            Text('The quation!'),
+            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: null),
+          ],
+        ),
+      ),
+    );
+  }
+}
