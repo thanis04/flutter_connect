4762d4c1a6c16e3d5b7e2844d5bfbbc7bf72434b
Thanis <77865201+thanis04@users.noreply.github.com>
2023-02-13 15:04:31 +0530

    function and something

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
+void main() => runApp(
+      DevicePreview(
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+    );
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionsIndex = 0;
+
+  void answerQuetion() {
+    questionsIndex = questionsIndex + 1;
+    print(questionsIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favvorite animale?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -15,10 +30,15 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The quation!'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
+            Text(questions[questionsIndex]),
+            ElevatedButton(child: Text('Answer 1'), onPressed: answerQuetion),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: () {
+                print('Answer choosen');
+              },
+            ),
+            ElevatedButton(child: Text('Answer 2'), onPressed: answerQuetion),
           ],
         ),
       ),
